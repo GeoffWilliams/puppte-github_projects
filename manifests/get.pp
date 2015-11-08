9b5bfbb4f9f4f55bfc8df6138ee1a046f68e8557
Geoff Williams <geoff@geoffwilliams.me.uk>
2015-11-08 20:14:46 +1100

    git auth support

--- manifests/get.pp
+++ manifests/get.pp
@@ -1,10 +1,12 @@
 define github_projects::get(
   $github_user,
   $local_user,
-  $base_dir = $title,
-  $script_dir = $github_projects::script_dir,
+  $base_dir    = $title,
+  $script_dir  = $github_projects::script_dir,
   $script_file = $github_projects::script_file,
-  $nopull = $github_projects::nopull,
+  $nopull      = $github_projects::nopull,
+  $token       = false,
+  $password    = false,
 ) {
 
   $script = "${script_dir}/${script_file}"
@@ -13,9 +15,17 @@ define github_projects::get(
     false => "",
   }
 
+  if $token {
+    $auth = "--token ${token}"
+  } elsif $password {
+    $auth = "--password ${password}"
+  } else {
+    fail("${module_name}::get { ${title}:} requires one of token or password to be set")
+  }
+
   # Run this every puppet run to fetch any new projects
   exec { "github_projects__get_${title}":
-    command => "${script} -u ${github_user} ${_nopull} --dest ${base_dir}",
+    command => "${script} -u ${github_user} ${_nopull} --dest ${base_dir} ${auth}",
     user    => $local_user,
     path    => [
         "/bin",
